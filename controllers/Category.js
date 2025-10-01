a87ec313db679aa57b8236cebfb7c302bc2f7294
Suryawanshiram <suryawanshiram2@gmail.com>
2025-10-01 14:56:18 +0530

    git commit -m "add: category, courseProgress , razorPay config created"

---
+++ controllers/Category.js
@@ -0,0 +1,125 @@
+const { Mongoose } = require("../config/database");
+const Category = require("../models/Category");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+exports.createCategory = async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    if (!name) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+    const categoryDetails = await Category.create({
+      name: name,
+      description: description,
+    });
+    console.log(categoryDetails);
+    return res.status(200).json({
+      success: true,
+      message: "Category Created Successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: true,
+      message: error.message,
+    });
+  }
+};
+
+exports.showAllCategories = async (req, res) => {
+  try {
+    const allCategories = await Category.find({});
+    console.log(allCategories);
+
+    res.status(200).json({
+      success: true,
+      data: allCategories,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//Category page details
+
+exports.categoryPageDetails = async (req, res) => {
+  try {
+    // get category by id
+    const { categoryId } = req.params;
+    // get courses by Specific categoryId
+    const selectedCategory = await Category.findById(categoryId)
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+        populate: "ratingAndReviews",
+      })
+      .exec();
+
+    // validation
+    if (!selectedCategory) {
+      console.log("Category not found.");
+      return res
+        .status(404)
+        .json({ success: false, message: "Category not found" });
+    }
+    // Handle the case when there are no courses
+    if (selectedCategory.courses.length === 0) {
+      console.log("No courses found for the selected category.");
+      return res.status(404).json({
+        success: false,
+        message: "No courses found for the selected category.",
+      });
+    }
+    //get courses for different categories
+    // Get courses for other categories
+    const categoriesExceptSelected = await Category.find({
+      _id: { $ne: categoryId },
+    });
+    let differentCategory = await Category.findOne(
+      categoriesExceptSelected[getRandomInt(categoriesExceptSelected.length)]
+        ._id
+    )
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+      })
+      .exec();
+    //console.log("Different COURSE", differentCategory)
+    // Get top-selling courses across all categories
+    const allCategories = await Category.find()
+      .populate({
+        path: "courses",
+        match: { status: "Published" },
+        populate: {
+          path: "instructor",
+        },
+      })
+      .exec();
+    const allCourses = allCategories.flatMap((category) => category.courses);
+    const mostSellingCourses = allCourses
+      .sort((a, b) => b.sold - a.sold)
+      .slice(0, 10);
+    // console.log("mostSellingCourses COURSE", mostSellingCourses)
+    res.status(200).json({
+      success: true,
+      data: {
+        selectedCategory,
+        differentCategory,
+        mostSellingCourses,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};

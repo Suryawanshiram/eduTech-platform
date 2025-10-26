258435c5e8898b07fc228221495875637115caba
Suryawanshiram <suryawanshiram2@gmail.com>
2025-10-26 08:46:57 +0530

    fix: cors policy fixed

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:5173",
       "https://edu-tech-frontend-indol.vercel.app",
-      "https://rs-edu-tech.vercel.app/",
+      "https://rs-edu-tech.vercel.app",
     ],
     credentials: true,
   })
